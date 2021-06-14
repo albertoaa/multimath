d1ab9b74d6cc6bb833776aac37a60b1c74734ffa
Alberto Aragon <alberto.alvarez@definedcrowd.com>
2021-06-14 10:05:31 +0100

    using webpack to build the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  entry: './app/app.ts',
-  devtool: 'inline-source-map',
+  entry: './app/app.ts', // Entry point of the app
+  devtool: 'inline-source-map', // Create source map inline with the bundle produced
   mode: 'development',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        use: 'ts-loader', // Run typescript compiler using a module
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: ['.tsx', '.ts', '.js'], // with files should be used for module resolution
   },
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.js', // name of the bundled file produced by webpack to sent to the browser
   },
   devServer: {
-    inline: false
-  }
+    inline: false, // keep the browser from automatically refresh when the bundle change, on production should be changed to true
+  },
 };
